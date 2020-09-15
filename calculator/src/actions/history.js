a5cd845c550a239904a5a1c5d27f0a12f01ba6a4
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-09-15 10:36:43 -0600

    Error:Made the action previously in reducer folder;transferred the content back to action folder;

---
+++ calculator/src/actions/history.js
@@ -0,0 +1,9 @@
+const history = (resultHistory) => {
+  return {
+    type: "HISTORY",
+    // This is the data we are passing through the reducer so that it can do its job
+    payload: resultHistory,
+  };
+};
+
+export default history;
