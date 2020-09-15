3005a36f534a4f752fb74a790737483a0a2a2467
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-09-15 11:53:31 -0600

    Add Provider and  Extension for Dev tools;

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -5,11 +5,15 @@ import Calculator from "./components/Calculator";
 import { createStore } from "redux";
 import historyReducer from "./reducers/history";
 import { history } from "./actions/history";
-// Connects global store-store to our entire Calculator-App(Helps in interact between react and redux store we created)
+// Connects global store-store to our entire Calculator-App
 import { Provider } from "react-redux";
 
 //Created a Global store Since store needs a reducer so we pass in the reducer in the store and import it
-const store = createStore(historyReducer);
+const store = createStore(
+  historyReducer,
+  //@Link :https://github.com/zalmoxisus/redux-devtools-extension
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 //For testing only--We can run the instructions every time the store is updated using stores subscribe method.
 store.subscribe(() => console.log(store.getState()));
 
@@ -17,7 +21,7 @@ store.subscribe(() => console.log(store.getState()));
 store.dispatch(history("Past Calculations"));
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <Calculator />,
   </Provider>,
   document.getElementById("root")
